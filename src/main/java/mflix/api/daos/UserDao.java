2d91cc86c724cb688f0171e67d8a72bfc0453c7a
Piotr Dopierala <ptr.dopierala@gmail.com>
2019-02-19 21:40:04 +0100

    added basic user management.

--- src/main/java/mflix/api/daos/UserDao.java
+++ src/main/java/mflix/api/daos/UserDao.java
@@ -5,6 +5,7 @@ import com.mongodb.MongoWriteException;
 import com.mongodb.WriteConcern;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.UpdateOptions;
 import com.mongodb.client.model.Updates;
 import com.mongodb.client.result.DeleteResult;
@@ -23,6 +24,7 @@ import org.springframework.context.annotation.Configuration;
 
 import java.text.MessageFormat;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
@@ -30,115 +32,144 @@ import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 @Configuration
 public class UserDao extends AbstractMFlixDao {
 
-  private final MongoCollection<User> usersCollection;
-  //TODO> Ticket: User Management - do the necessary changes so that the sessions collection
-  //returns a Session object
-  private final MongoCollection<Document> sessionsCollection;
+    private final MongoCollection<User> usersCollection;
+    //TODO> Ticket: User Management - do the necessary changes so that the sessions collection
+    //returns a Session object
+    private final MongoCollection<Document> sessionsCollection;
 
-  private final Logger log;
+    private final Logger log;
 
-  @Autowired
-  public UserDao(
-      MongoClient mongoClient, @Value("${spring.mongodb.database}") String databaseName) {
-    super(mongoClient, databaseName);
-    CodecRegistry pojoCodecRegistry =
-        fromRegistries(
-            MongoClientSettings.getDefaultCodecRegistry(),
-            fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+    @Autowired
+    public UserDao(
+            MongoClient mongoClient, @Value("${spring.mongodb.database}") String databaseName) {
+        super(mongoClient, databaseName);
+        CodecRegistry pojoCodecRegistry =
+                fromRegistries(
+                        MongoClientSettings.getDefaultCodecRegistry(),
+                        fromProviders(PojoCodecProvider.builder().automatic(true).build()));
 
-    usersCollection = db.getCollection("users", User.class).withCodecRegistry(pojoCodecRegistry);
-    log = LoggerFactory.getLogger(this.getClass());
-    //TODO> Ticket: User Management - implement the necessary changes so that the sessions
-    // collection returns a Session objects instead of Document objects.
-    sessionsCollection = db.getCollection("sessions");
-  }
+        usersCollection = db.getCollection("users", User.class).withCodecRegistry(pojoCodecRegistry);
+        log = LoggerFactory.getLogger(this.getClass());
+        //TODO> Ticket: User Management - implement the necessary changes so that the sessions
+        // collection returns a Session objects instead of Document objects.
+        sessionsCollection = db.getCollection("sessions");
+    }
 
-  /**
-   * Inserts the `user` object in the `users` collection.
-   *
-   * @param user - User object to be added
-   * @return True if successful, throw IncorrectDaoOperation otherwise
-   */
-  public boolean addUser(User user) {
-    //TODO > Ticket: Durable Writes -  you might want to use a more durable write concern here!
-    usersCollection.insertOne(user);
-    return true;
-    //TODO > Ticket: Handling Errors - make sure to only add new users
-    // and not users that already exist.
+    /**
+     * Inserts the `user` object in the `users` collection.
+     *
+     * @param user - User object to be added
+     * @return True if successful, throw IncorrectDaoOperation otherwise
+     */
+    public boolean addUser(User user) {
+        //TODO > Ticket: Durable Writes -  you might want to use a more durable write concern here!
+        usersCollection.insertOne(user);
+        return true;
+        //TODO > Ticket: Handling Errors - make sure to only add new users
+        // and not users that already exist.
 
-  }
+    }
 
-  /**
-   * Creates session using userId and jwt token.
-   *
-   * @param userId - user string identifier
-   * @param jwt - jwt string token
-   * @return true if successful
-   */
-  public boolean createUserSession(String userId, String jwt) {
-    //TODO> Ticket: User Management - implement the method that allows session information to be
-    // stored in it's designated collection.
-    return false;
-    //TODO > Ticket: Handling Errors - implement a safeguard against
-    // creating a session with the same jwt token.
-  }
+    /**
+     * Creates session using userId and jwt token.
+     *
+     * @param userId - user string identifier
+     * @param jwt    - jwt string token
+     * @return true if successful
+     */
+    public boolean createUserSession(String userId, String jwt) {
+        //TODO> Ticket: User Management - implement the method that allows session information to be
+        // stored in it's designated collection.
 
-  /**
-   * Returns the User object matching the an email string value.
-   *
-   * @param email - email string to be matched.
-   * @return User object or null.
-   */
-  public User getUser(String email) {
-    User user = null;
-    //TODO> Ticket: User Management - implement the query that returns the first User object.
-    return user;
-  }
+        Document doc = new Document("user_id", userId);
+        doc.put("jwt", jwt);
+        sessionsCollection.insertOne(doc);
+        return true;
+        //TODO > Ticket: Handling Errors - implement a safeguard against
+        // creating a session with the same jwt token.
+    }
 
-  /**
-   * Given the userId, returns a Session object.
-   *
-   * @param userId - user string identifier.
-   * @return Session object or null.
-   */
-  public Session getUserSession(String userId) {
-    //TODO> Ticket: User Management - implement the method that returns Sessions for a given
-    // userId
-    return null;
-  }
+    /**
+     * Returns the User object matching the an email string value.
+     *
+     * @param email - email string to be matched.
+     * @return User object or null.
+     */
+    public User getUser(String email) {
+        User user = null;
+        //TODO> Ticket: User Management - implement the query that returns the first User object.
+        Document queryFilter = new Document("email", email);
+        try {
+            user = usersCollection.find(queryFilter).limit(1).iterator().next();
+            return user;
+        }catch (NoSuchElementException noElEx){
+            return null;
+        }
+    }
 
-  public boolean deleteUserSessions(String userId) {
-    //TODO> Ticket: User Management - implement the delete user sessions method
-    return false;
-  }
+    /**
+     * Given the userId, returns a Session object.
+     *
+     * @param userId - user string identifier.
+     * @return Session object or null.
+     */
+    public Session getUserSession(String userId) {
+        //TODO> Ticket: User Management - implement the method that returns Sessions for a given
+        // userId
+        Document queryFilter = new Document("user_id", userId);
+        try {
+            Document next = sessionsCollection.find(queryFilter).limit(1).iterator().next();
+            if (!next.isEmpty()) {
+                Session session = new Session();
+                session.setJwt(next.get("jwt").toString());
+                session.setUserId(next.get("user_id").toString());
+                return session;
+            } else
+                return null;
+        }catch (NoSuchElementException noElEx){
+            return null;
+        }
+    }
 
-  /**
-   * Removes the user document that match the provided email.
-   *
-   * @param email - of the user to be deleted.
-   * @return true if user successfully removed
-   */
-  public boolean deleteUser(String email) {
-    // remove user sessions
-    //TODO> Ticket: User Management - implement the delete user method
-    //TODO > Ticket: Handling Errors - make this method more robust by
-    // handling potential exceptions.
-    return false;
-  }
+    public boolean deleteUserSessions(String userId) {
+        //TODO> Ticket: User Management - implement the delete user sessions method
+        Bson query = Filters.eq("user_id", userId);
+        DeleteResult dResult = sessionsCollection.deleteOne(query);
+        return dResult.wasAcknowledged();
+    }
 
-  /**
-   * Updates the preferences of an user identified by `email` parameter.
-   *
-   * @param email - user to be updated email
-   * @param userPreferences - set of preferences that should be stored and replace the existing
-   *     ones. Cannot be set to null value
-   * @return User object that just been updated.
-   */
-  public boolean updateUserPreferences(String email, Map<String, ?> userPreferences) {
-    //TODO> Ticket: User Preferences - implement the method that allows for user preferences to
-    // be updated.
-    //TODO > Ticket: Handling Errors - make this method more robust by
-    // handling potential exceptions when updating an entry.
-    return false;
-  }
+    /**
+     * Removes the user document that match the provided email.
+     *
+     * @param email - of the user to be deleted.
+     * @return true if user successfully removed
+     */
+    public boolean deleteUser(String email) {
+        // remove user sessions
+        //TODO> Ticket: User Management - implement the delete user method
+        //TODO > Ticket: Handling Errors - make this method more robust by
+        // handling potential exceptions.
+
+        deleteUserSessions(email);
+
+        Bson query = Filters.eq("email", email);
+        DeleteResult dResult = usersCollection.deleteOne(query);
+        return dResult.wasAcknowledged();
+    }
+
+    /**
+     * Updates the preferences of an user identified by `email` parameter.
+     *
+     * @param email           - user to be updated email
+     * @param userPreferences - set of preferences that should be stored and replace the existing
+     *                        ones. Cannot be set to null value
+     * @return User object that just been updated.
+     */
+    public boolean updateUserPreferences(String email, Map<String, ?> userPreferences) {
+        //TODO> Ticket: User Preferences - implement the method that allows for user preferences to
+        // be updated.
+        //TODO > Ticket: Handling Errors - make this method more robust by
+        // handling potential exceptions when updating an entry.
+        return false;
+    }
 }
--- src/test/java/mflix/lessons/BasicWrites.java
+++ src/test/java/mflix/lessons/BasicWrites.java
@@ -22,19 +22,10 @@ import java.util.List;
 @SpringBootTest
 public class BasicWrites extends AbstractLesson {
 
-  /**
-   * In this lesson we are going to look into writing new data into a collection. Specifically, we
-   * are going to review the insert, insertMany and update with {upsert: true} options in the Java
-   * driver.
-   */
   private MongoCollection<Document> videoGames;
 
   @Before
   public void setUp() {
-    /*
-     * We start by instantiating a collection, called videoGames, in the
-     * test database.
-     */
 
     videoGames = testDb.getCollection("video_games");
   }
@@ -42,48 +33,18 @@ public class BasicWrites extends AbstractLesson {
   @Test
   public void testWriteOneDocument() {
 
-    /*
-     * Once we have a collection to write to let's create a Document object
-     * instance that will hold some information. In this case, we will
-     * have a document that represents a video game.
-     */
-
     // we set the first key, right in the constructor.
     Document doc = new Document("title", "Fortnite");
 
-    // then we add a new set of document fields like year of the game launch
-    // by appending a new key value pair
     doc.append("year", 2018);
-
-    // and another one using put()
     doc.put("label", "Epic Games");
 
     // then we can insert this document by calling the collection insertOne
     // method
     videoGames.insertOne(doc);
 
-    /*
-     * As you probably noticed, the insertOne method returns void. So
-     * how do we know if the document has been correctly inserted?
-     * If an error occurred, during the insert of a given document, a
-     * MongoWriteException would be thrown stating the origin/cause of
-     * the error. In later lessons, we will look into which kind of
-     * exceptions we should be prepared to handled and what causes such
-     * exceptions.
-     * Otherwise, everything get's correctly written and the document is
-     * stored in the database.
-     * However, the avid MongoDB expert in you is wondering, where can we
-     * see the _id field? We did not set one, so surely it would still
-     * need to be set. And you are right.
-     */
-
     Assert.assertNotNull(doc.getObjectId("_id"));
-    // On insert, the driver will set a _id field if it is not set by the
-    // application, with the default datatype of ObjectId
-    // https://docs.mongodb.com/manual/reference/method/ObjectId/index.html
 
-    // This basically means that if we try to recover the document back from
-    // the database, using this _id value, we will get the same document
     Document retrieved = videoGames.find(Filters.eq("_id", doc.getObjectId("_id"))).first();
 
     // Which we can assert that it is true
@@ -92,12 +53,6 @@ public class BasicWrites extends AbstractLesson {
 
   @Test
   public void testInsertMany() {
-    /*
-     * Another option to insert new data into a collection, is to insert it
-     * in bulk, or better saying several documents at once.
-     */
-
-    // In this case I'm going to set up a list of two documents
 
     List<Document> someGames = new ArrayList<>();
 
@@ -112,19 +67,11 @@ public class BasicWrites extends AbstractLesson {
     documentValues.put("year", 2013);
     doc2.putAll(documentValues);
 
-    // Once we have the two documents we can add them to the list of
-    // documents
     someGames.add(doc1);
     someGames.add(doc2);
 
     // and finally insert all of these documents using insertMany
     videoGames.insertMany(someGames);
-
-    /*
-     * The same logic applies as in insertOne, if an error occurs that
-     * prevents the documents to be inserted, we would need to capture a
-     * Runtime exception.
-     */
 
     // If we look back into the object references we can see that the _id
     // fields are correctly set
@@ -141,59 +88,28 @@ public class BasicWrites extends AbstractLesson {
 
   @Test
   public void testUpsertDocument() {
-    /*
-     * There are times where we are not sure if a document already exists
-     * in the collection, and we just want to update it if it already
-     * exists. Something like with "update" or "insert" if it does not
-     * exist. Well, MongoDB allows for that in a very straightforward way.
-     */
 
     // Let's go ahead and instantiate our document
     Document doc1 = new Document("title", "Final Fantasy");
     doc1.put("year", 2003);
     doc1.put("label", "Square Enix");
 
-    // and instead of going to the database, run a query to check if the
-    // document already exists, we are going to emit an update command
-    // with the flag $upsert: true.
-
-    // We set a query predicate that finds the video game based on his title
     Bson query = new Document("title", "Final Fantasy");
 
-    // And we try to updated. If we do not provide the upsert flag
     UpdateResult resultNoUpsert = videoGames.updateOne(query, new Document("$set", doc1));
-
-    // if the document does not exist, the number of matched documents
     Assert.assertEquals(0, resultNoUpsert.getMatchedCount());
-    // should be 0, so as the number of modified documents.
     Assert.assertNotEquals(1, resultNoUpsert.getModifiedCount());
 
-    // on the other hand, if we do provide an upsert flag by setting the
-    // UpdateOptions document
     UpdateOptions options = new UpdateOptions();
     options.upsert(true);
 
-    // and adding those options to the update method.
     UpdateResult resultWithUpsert =
         videoGames.updateOne(query, new Document("$set", doc1), options);
 
-    // in this case both our number of modified count
     Assert.assertEquals(0, resultWithUpsert.getModifiedCount());
-    // should be still 0 given that there was no document in the
-    // collection, however we do have a upsertId as result of the insert
     Assert.assertNotNull(resultWithUpsert.getUpsertedId());
-    // and should be of ObjectId type
     Assert.assertTrue(resultWithUpsert.getUpsertedId().isObjectId());
 
-    // Another component of the update or insert, upsert, is that we can
-    // set values just in case we are inserting.
-    // Let's say we want add a field called, just_inserted, if the
-    // document did not existed before, but do not set it if the document
-    // already existed
-
-    // let's try to update "Final Fantasy", which already exists:
-    // we need to setup an object that defines the update operation,
-    // set's the title and appends the field just_inserted with string "yes"
     Bson updateObj1 =
         Updates.combine(
             Updates.set("title", "Final Fantasy 1"), Updates.setOnInsert("just_inserted", "yes"));
@@ -202,8 +118,6 @@ public class BasicWrites extends AbstractLesson {
 
     UpdateResult updateAlreadyExisting = videoGames.updateOne(query, updateObj1, options);
 
-    // In this case, the field will not be present when we query for this
-    // document
     Document finalFantasyRetrieved =
         videoGames.find(Filters.eq("title", "Final Fantasy 1")).first();
     Assert.assertFalse(finalFantasyRetrieved.keySet().contains("just_inserted"));
@@ -229,12 +143,6 @@ public class BasicWrites extends AbstractLesson {
     Assert.assertEquals("yes", csgoDocument.get("just_inserted"));
   }
 
-  /**
-   * Let's recap: - we can insert new documents using both the insertOne or insertMany collection
-   * methods - update using the $upsert flag set to true also allows us to insert new documents -
-   * using $setOnInsert update operator provides a way to set specific fields only in the case of
-   * insert.
-   */
   @After
   public void tearDown() {
     this.videoGames.drop();
